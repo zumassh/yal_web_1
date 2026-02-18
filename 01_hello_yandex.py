abfb9083789c912952bdd6ac074a531bcf4b07c2
zumassh <stasy.sl@yandex.ru>
2026-02-18 22:02:48 +0300

    feat: добавлен js. Для стартовой страницы он встроен в html - в теге script, для страницы /hello/... скрипт подключен в теге <head> через url_for (аналогично картинкам и стилям). Скрипты js должны лежать в папке static/js.

--- 01_hello_yandex.py
+++ 01_hello_yandex.py
@@ -16,8 +16,27 @@ def page():
            <title>Привет, Яндекс!</title>
          </head>
          <body>
-            <p><i>Добро пожаловать в Яндекс Лицей!</i></p>
-            <img src="{url_for('static', filename='img/yal.png')}" alt="Добро пожаловать в Яндекс Лицей!">
+         <div class="mainContainer">
+            <div>
+                <p><i>Добро пожаловать в Яндекс Лицей!</i></p>
+                <img src="{url_for('static', filename='img/yal.png')}" alt="Добро пожаловать в Яндекс Лицей!">\
+            </div>
+            <div class="interactContainer">
+                <div id="interactElement" style="padding: 20px; margin: 10px; background-color: lightblue;">
+                    Сейчас что-то будет...
+                </div>
+                <button id="actionButton">Нажми меня</button>
+                <script>   // встроенный js-блок
+                    document.getElementById('actionButton').onclick = function() {{ // получаем кнопку по её id
+                        const el = document.getElementById('interactElement');  // получаем элемент для изменения по id
+                        el.style.backgroundColor = '#4CAF50';   // меняем стиль элемента
+                        el.style.color = 'white';
+                        el.style.transform = 'scale(1.1)';
+                        el.innerHTML = "Всё изменилось.";   // меняем текстовое содержимое элемента
+                    }};
+                </script>
+            </div>
+         </div>
          </body>
        </html>
     '''
@@ -26,19 +45,41 @@ def page():
 @app.route('/hello/<string:username>')
 def hello(username):
     age = request.args.get('age', 16)
+    url_style = url_for('static', filename='css/style.css')
     return f'''
     <! doctype html>
     <html lang="en">
         <head>
             <meta charset="utf-8">
             <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+            <link rel="stylesheet" type="text/css" href="{url_style}">
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+            
+            <script src="{ url_for('static', filename='js/script.js') }">                 // загружаем файл со скриптом js
+            </script>
         </head>
         <body>
             <h1>Добрый день, {username}, {age}!</h1>
-            <h2 class="alert alert-primary d-flex align-items-center">Ваша группа - 5!</h1>
-            <h3 class="alert alert-success d-flex align-items-center">Площадка - ДТ "Альтаир"</h2>
-            <h4 class="alert alert-info d-flex align-items-center">Преподаватель - Слободянюк Анастасия Андреевна</h3>
+            <div id="interactiveGroup">
+                <h2 class="alert alert-primary d-flex align-items-center" id="group">Ваша группа - 5!</h2>
+                <h3 class="alert alert-success d-flex align-items-center" id="place">Площадка - ДТ "Альтаир"</h3>
+                <h4 class="alert alert-info d-flex align-items-center" id="teacher">Преподаватель - Слободянюк Анастасия Андреевна</h4>
+            </div>
+            <div>
+                <form class="jsForm">
+                    <label for="group">Изменить группу</label>
+                    <input placeholder="введите группу" name="groupInput" id="groupInput">
+                    
+                    <label for="place">Изменить площадку</label>
+                    <input placeholder="введите группу" name="placeInput" id="placeInput">
+                    
+                    <label for="teacher">Изменить преподавателя</label>
+                    <input placeholder="введите группу" name="teacherInput" id="teacherInput">
+                    
+                    <!-- атрибут onClick нужен для определения функции из js, которая сработает при клике на кнопку -->
+                    <button type="button" onClick="changeAll()">изменить всё!</button>
+                </form>
+            </div>
         </body>
     </html>
     '''
